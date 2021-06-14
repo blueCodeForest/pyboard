e4d70d0940838543f95160eed6668d269b5a60bb
blueCodeForest <tokinari.pp@gmail.com>
2021-06-14 18:33:10 +0900

    added heroku files

--- app/settings.py
+++ app/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 import environ
+import django_heroku
 
 from django.utils.translation import ugettext_lazy as _
 
@@ -10,7 +11,12 @@ LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'),)
 
 # 環境変数の読み込み
 env = environ.Env(DEBUG=(bool,False))
-env.read_env(os.path.join(BASE_DIR,'.env'))
+
+IS_ON_HEROKU = env.bool('ON_HEROKU', default=False)
+
+if not IS_ON_HEROKU:
+    env.read_env(os.path.join(BASE_DIR,'.env'))
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
@@ -21,7 +27,7 @@ DEBUG = env.get_value('DEBUG', cast = bool, default = True)
 if DEBUG:
     ALLOWED_HOSTS = ['*']
 else:
-    ALLOWED_HOSTS = ['xxx.com', 'www.xxx.com', 'localhost']
+    ALLOWED_HOSTS = ['pyboard-5616.herokuapp.com']
 
 
 INSTALLED_APPS = [
@@ -43,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls' #プロジェクト名をxxxに入れる
@@ -131,10 +138,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/')
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media/'
@@ -147,3 +156,4 @@ FILE_UPLOAD_MAX_MEMORY_SIZE = 15728640
 DATA_UPLOAD_MAX_MEMORY_SIZE = 15728640
 
 
+django_heroku.settings(locals())
